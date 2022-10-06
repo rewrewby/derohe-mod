c7bc9828e265d306ceb9952768c8c7b6e7c9dd3c
Hansen33 <root@mysrv.cloud>
2022-10-07 00:49:45 +0100

    Cleaning up duplication

--- cmd/derod/rpc/miner_stats.go
+++ cmd/derod/rpc/miner_stats.go
@@ -4,13 +4,8 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
-	"sync/atomic"
-	"time"
 
-	"github.com/deroproject/derohe/block"
-	"github.com/deroproject/derohe/config"
 	"github.com/deroproject/derohe/globals"
-	"github.com/deroproject/derohe/p2p"
 )
 
 type inner_miner_stats struct {
@@ -125,107 +120,4 @@ func getMinerStats(wallet string) (stats inner_miner_stats) {
 	stats = miner_stats[wallet]
 
 	return stats
-}
-
-func CheckIfMiniBlockIsOrphaned(local bool, mblData block.MiniBlock, miner string) {
-
-	var fails int
-
-	for {
-		time.Sleep(time.Second)
-		if chain.Get_Height() >= int64(mblData.Height)+1 {
-			// logger.V(2).Info(fmt.Sprintf("Height: %d - Checking if orphaned (%s)", mblData.Height, mblData.GetHash().String()))
-			block, err := chain.Load_Block_Topological_order_at_index(int64(mblData.Height))
-			if err != nil {
-				fails++
-				if fails == 5 {
-					return
-				}
-				continue
-			}
-			bl, err := chain.Load_BL_FROM_ID(block)
-			if err != nil {
-				fails++
-				if fails == 5 {
-					return
-				}
-				continue
-			}
-			for _, mbl := range bl.MiniBlocks {
-				if mbl == mblData {
-					return
-				}
-			}
-
-			// Mini Block is Orphan
-
-			logger.V(2).Info(fmt.Sprintf("Height: %d - %s Miniblock (%s) ORPHANED", mblData.Height, miner, mblData.GetHash().String()))
-
-			if local {
-				logger.Info(fmt.Sprintf(red+"Height: %d"+reset_color+" - "+red+"%s"+reset_color+": "+blue+"Orphan DERO mini block\t"+yellow+"("+red+"Profit Loss 💣"+yellow+")"+reset_color+reset_color, mblData.Height, miner))
-			} else {
-				if config.RunningConfig.TraceBlocks {
-					logger.Info(fmt.Sprintf(red+"Height: %d"+reset_color+" - "+red+"%s"+reset_color+": "+blue+"Orphan DERO mini block"+reset_color, mblData.Height, miner))
-				}
-			}
-
-			if local {
-				atomic.AddInt64(&globals.CountOrphanMinis, 1)
-				go p2p.AddBlockToMyOrphanMiniBlockCollection(mblData, miner)
-			}
-			go p2p.AddBlockToOrphanMiniBlockCollection(mblData, miner)
-
-			return
-		}
-	}
-}
-
-func CheckIfBlockIsOrphaned(local bool, blockData block.MiniBlock, miner string) {
-
-	var fails int
-
-	for {
-		time.Sleep(time.Second)
-		if chain.Get_Height() >= int64(blockData.Height)+1 {
-			// logger.V(2).Info(fmt.Sprintf("Height: %d - Checking if orphaned (%s)", mblData.Height, mblData.GetHash().String()))
-			block_id, err := chain.Load_Block_Topological_order_at_index(int64(blockData.Height))
-			if err != nil {
-				fails++
-				if fails == 5 {
-					return
-				}
-				continue
-			}
-
-			bl, err := chain.Load_BL_FROM_ID(block_id)
-			if err != nil {
-				logger.Error(err, "loading block", "blid", block_id, "height", blockData.Height)
-			}
-
-			// Is not orphan
-			if bl.MiniBlocks[9] == blockData {
-				// logger.V(2).Info(fmt.Sprintf("Height: %d - %s Integrator block (%s) NOT ORPHANED", blockData.Height, miner, blockData.GetHash().String()))
-				return
-			}
-
-			logger.V(2).Info(fmt.Sprintf("Height: %d - %s Integrator block (%s) ORPHANED", blockData.Height, miner, blockData.GetHash().String()))
-
-			if local {
-				logger.Info(fmt.Sprintf(red+"Height: %d"+reset_color+" - "+red+"%s"+reset_color+": "+blue+"Orphan DERO integrator block\t"+yellow+"("+red+"Profit Loss 💣"+yellow+")"+reset_color+reset_color, blockData.Height, miner))
-			} else {
-				if config.RunningConfig.TraceBlocks {
-					logger.Info(fmt.Sprintf(red+"Height: %d"+reset_color+" - "+red+"%s"+reset_color+": "+blue+"Orphan DERO integrator block"+reset_color, blockData.Height, miner))
-				}
-			}
-
-			if local {
-				atomic.AddInt64(&globals.CountOrphanBlocks, 1)
-				// Update Miner
-				go p2p.AddBlockToMyOrphanBlockCollection(blockData, miner)
-			}
-			go p2p.AddBlockToOrphanBlockCollection(blockData, miner)
-
-			return
-		}
-	}
 }
